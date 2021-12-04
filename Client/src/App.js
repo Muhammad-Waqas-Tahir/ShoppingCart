eb71fc86cf8b28aa993135d777d6efacf899a80e
Muhammad-Waqas-Tahir <71683137+Muhammad-Waqas-Tahir@users.noreply.github.com>
2021-12-04 21:12:36 +0500

    commit

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import Home from "./component/Home";
+import Cart from "./component/Cart";
+import NotFound from "./component/NotFound";
 function App() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/notfound" element={<NotFound />} />
+          {/* <Route path="*" element={<NotFound />} /> */}
+          <Route path="*" element={<Navigate to="/notfound" />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ api/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import products from "./product.js";
+
+const app = express();
+app.use(express(JSON));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("weclome to my api");
+});
+app.get("/product", (req, res) => {
+  res.send(products);
+});
+
+const Port = process.env.PORT || 5000;
+
+app.listen(Port, console.log(`Server is running on Port ${Port}`));
